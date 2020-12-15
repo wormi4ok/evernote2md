e9b9c5b7ef73167e3095034edf69b5276e62c800
Stanislav Petrashov <s@petrashov.ru>
2020-12-15 22:35:27 +0100

    Implement ChangeFileTimes function for Windows

--- file/rules.go
+++ file/rules.go
@@ -2,10 +2,42 @@
 
 package file
 
-import "regexp"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"time"
+)
 
 // Max path length is 1000 - 9 bytes for extension (.md) in multibyte encoding
 const maxPathLength int = 991
 
 // Semicolon is not allowed in MacOS and spaces is just my personal preference
 var illegalChars = regexp.MustCompile(`[\s:]`)
+
+// Match the file times with the Evernote metadata
+//
+// Uses touch if available to change both creation  and modification date
+// Otherwise it falls back to os.Chtimes to change only the modification date
+func ChangeFileTimes(dir, name string, ctime, mtime time.Time) error {
+	filePathToModify := filepath.FromSlash(dir + "/" + name)
+	if _, err := os.Stat(filePathToModify); os.IsNotExist(err) {
+		return fmt.Errorf("change file timestamps %s: %w", filePathToModify, err)
+	}
+
+	touchCmd, err := exec.LookPath("touch")
+	if err != nil {
+		return os.Chtimes(filePathToModify, mtime, mtime)
+	}
+	changeMtime := exec.Command(touchCmd, "-mt", mtime.Format(touchTimeFormat), filePathToModify)
+	if err := changeMtime.Run(); err != nil {
+		return err
+	}
+	changeCTime := exec.Command(touchCmd, "-t", ctime.Format(touchTimeFormat), filePathToModify)
+	if err := changeCTime.Run(); err != nil {
+		return err
+	}
+	return nil
+}
--- file/rules_windows.go
+++ file/rules_windows.go
@@ -1,9 +1,31 @@
 package file
 
-import "regexp"
+import (
+	"os"
+	"path/filepath"
+	"regexp"
+	"syscall"
+	"time"
+)
 
 // Max path length is 255 - 9 bytes for extension (.md) in multibyte encoding
 const maxPathLength int = 246
 
 // Additional rule for
 var illegalChars = regexp.MustCompile(`[\s\\|"'<>&_=+:?*]`)
+
+// ChangeFileTimes uses SetFileTime syscall in Windows implementation
+// which supports updating both creation and modification dates
+func ChangeFileTimes(dir, name string, ctime, mtime time.Time) error {
+	path := filepath.FromSlash(dir + "/" + name)
+	ctimeSpec := syscall.NsecToFiletime(ctime.UnixNano())
+	mtimeSpec := syscall.NsecToFiletime(mtime.UnixNano())
+
+	fd, err := syscall.Open(path, os.O_RDWR, 0755)
+	if err != nil {
+		return err
+	}
+	defer syscall.Close(fd)
+
+	return syscall.SetFileTime(fd, &ctimeSpec, &mtimeSpec, &mtimeSpec)
+}
