d115248bb8aeb89663cb1c042b4ddd54c3ae1bb4
Stanislav Petrashov <s@petrashov.ru>
2021-01-09 20:35:14 +0100

    Refactor Converter main function to improve readability

--- internal/convert.go
+++ internal/convert.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/wormi4ok/evernote2md/encoding/enex"
@@ -18,46 +17,35 @@ import (
 type Converter struct {
 	TagFormat        string
 	EnableHighlights bool
+
+	// err holds an error during conversion
+	// Every conversion step should check this field and skip execution if it is not empty
+	err error
 }
 
-// Convert Evernote file to markdown
+// Convert an Evernote file to markdown
 func (c *Converter) Convert(note *enex.Note) (*markdown.Note, error) {
-	var md markdown.Note
+	md := new(markdown.Note)
 	md.Media = map[string]markdown.Resource{}
 
-	if err := mapResources(note, md); err != nil {
-		return nil, err
-	}
+	c.mapResources(note, md)
+	c.normalizeHTML(note, md, NewReplacerMedia(md.Media), &Code{}, &ExtraDiv{}, &TextFormatter{})
+	c.prependTags(note, md)
+	c.prependTitle(note, md)
+	c.toMarkdown(note, md)
+	c.trimSpaces(note, md)
+	c.addDates(note, md)
 
-	html, err := normalizeHTML(note.Content, NewReplacerMedia(md.Media), &Code{}, &ExtraDiv{}, &TextFormatter{})
-	if err != nil {
-		return nil, err
-	}
-
-	content := prependTags(note.Tags, string(html))
-	content = prependTitle(note.Title, content)
-
-	var b bytes.Buffer
-	err = markdown.Convert(&b, strings.NewReader(content), c.EnableHighlights)
-	if err != nil {
-		return nil, err
-	}
-
-	md.Content = regexp.MustCompile(`\n{3,}`).ReplaceAllLiteral(b.Bytes(), []byte("\n\n"))
-	md.Content = append(bytes.TrimRight(md.Content, "\n"), '\n')
-
-	md.CTime = convertEvernoteDate(note.Created)
-	md.MTime = convertEvernoteDate(note.Updated)
-	return &md, nil
+	return md, c.err
 }
 
-func mapResources(note *enex.Note, md markdown.Note) error {
+func (c *Converter) mapResources(note *enex.Note, md *markdown.Note) {
 	names := map[string]int{}
 	r := note.Resources
 	for i := range r {
 		p, err := ioutil.ReadAll(decoder(r[i].Data))
-		if err != nil {
-			return err
+		if c.err = err; err != nil {
+			return
 		}
 
 		rType := markdown.File
@@ -85,21 +73,58 @@ func mapResources(note *enex.Note, md markdown.Note) error {
 		} else {
 			md.Media[strconv.Itoa(i)] = mdr
 		}
-
 	}
-	return nil
 }
 
-func prependTags(tags []string, content string) string {
-	var tt []string
-	for _, t := range tags {
-		tt = append(tt, fmt.Sprintf("<code>%s</code>", t))
+func (c *Converter) prependTags(note *enex.Note, _ *markdown.Note) {
+	if c.err != nil {
+		return
 	}
-	return strings.Join(tt, " ") + "<br>" + content
+
+	var tt [][]byte
+	for _, t := range note.Tags {
+		tt = append(tt, []byte(fmt.Sprintf("<code>%s</code>", t)))
+	}
+	note.Content = append([]byte("<br>"), note.Content...)
+	note.Content = append(bytes.Join(tt, []byte(" ")), note.Content...)
 }
 
-func prependTitle(title, content string) string {
-	return fmt.Sprintf("<h1>%s</h1>", title) + content
+func (c *Converter) prependTitle(note *enex.Note, _ *markdown.Note) {
+	if c.err != nil {
+		return
+	}
+
+	note.Content = append([]byte(fmt.Sprintf("<h1>%s</h1>", note.Title)), note.Content...)
+}
+
+func (c *Converter) toMarkdown(note *enex.Note, md *markdown.Note) {
+	if c.err != nil {
+		return
+	}
+	var b bytes.Buffer
+	err := markdown.Convert(&b, bytes.NewReader(note.Content), c.EnableHighlights)
+	if c.err = err; err != nil {
+		return
+	}
+
+	md.Content = b.Bytes()
+}
+
+func (c *Converter) trimSpaces(_ *enex.Note, md *markdown.Note) {
+	if c.err != nil {
+		return
+	}
+
+	md.Content = regexp.MustCompile(`\n{3,}`).ReplaceAllLiteral(md.Content, []byte("\n\n"))
+	md.Content = append(bytes.TrimRight(md.Content, "\n"), '\n')
+}
+
+func (c *Converter) addDates(note *enex.Note, md *markdown.Note) {
+	if c.err != nil {
+		return
+	}
+	md.CTime = convertEvernoteDate(note.Created)
+	md.MTime = convertEvernoteDate(note.Updated)
 }
 
 const evernoteDateFormat = "20060102T150405Z"
