9d0ae038cc25b56b86db7cafb327038bb596b3d1
Leland Krych <lkrych@cisco.com>
2021-01-09 11:53:24 -0800

    add textformatting struct and replace tag rules

--- internal/replace.go
+++ internal/replace.go
@@ -180,3 +180,42 @@ func hasExtraDiv(n *html.Node) bool {
 
 	return false
 }
+
+// TextFormatter catches Bold, Italic or underlined formatting
+type TextFormatter struct{}
+
+// ReplaceTag implements the TagReplacer interface
+func (*TextFormatter) ReplaceTag(n *html.Node) {
+	if isBold(n) {
+		n.Data = "strong"
+		n.Attr = []html.Attribute{}
+	}
+	if isItalic(n) {
+		n.Data = "i"
+		n.Attr = []html.Attribute{}
+	}
+}
+
+func isBold(n *html.Node) bool {
+	if n.Type == html.ElementNode && n.Data == "span" {
+		for _, a := range n.Attr {
+			if a.Key == "style" {
+				return strings.Contains(a.Val, "font-weight: bold")
+			}
+		}
+	}
+
+	return false
+}
+
+func isItalic(n *html.Node) bool {
+	if n.Type == html.ElementNode && n.Data == "span" {
+		for _, a := range n.Attr {
+			if a.Key == "style" {
+				return strings.Contains(a.Val, "font-style: italic")
+			}
+		}
+	}
+
+	return false
+}
